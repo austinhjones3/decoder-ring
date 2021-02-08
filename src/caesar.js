0084314cf7c93e006921821b523feb30eef92a89
austinhjones3 <austin@austinjones.io>
2021-02-08 14:43:37 -0600

    src/caesar and test/ceaser.test first commit

--- src/caesar.js
+++ src/caesar.js
@@ -7,8 +7,36 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    // make sure arguments are good. return false if not
+    if (!input) return false;
+    if (shift === 0 || shift < -25 || shift > 25 || !shift) return false;
+    // if encode is false, turn shift to it's negative value to decode
+    if (!encode) shift = -shift;
+    // move all input chars to lowercase as per req specs
+    input = input.toLowerCase();
+
+    // character accumulator
+    let result = "";
+    for (let i = 0; i < input.length; i++) {
+      // get ascii value of current char
+      const current = input.charCodeAt(i);
+      // only shift chars that are letters
+      if (current >= 97 && current <= 122) {
+        // newChar is an ascii code shifted
+        let newChar = ((current - 97 + shift) % 26) + 97;
+        // lock newChar's value between 97 and 122
+        if (newChar < 97) newChar += 26;
+        if (newChar > 122) newChar -= 26;
+        // push the character for the ascii code to the result string
+        result += String.fromCharCode(newChar);
+      } else {
+        result += String.fromCharCode(current);
+      }
+    }
+    return result;
   }
+  // console.log(caesar("Hello World", -25, true)); //> khoor zruog
+  // console.log(caesar("ifmmp xpsme", -25, false));
 
   return {
     caesar,
