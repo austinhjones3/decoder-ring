664500704d178725e47b60fc56efbf35796d30ff
austinhjones3 <austin@austinjones.io>
2021-02-08 18:47:45 -0600

    polybius default tests passing

--- src/polybius.js
+++ src/polybius.js
@@ -4,12 +4,70 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  const square = [
+    { letter: "A", number: "11" },
+    { letter: "B", number: "21" },
+    { letter: "C", number: "31" },
+    { letter: "D", number: "41" },
+    { letter: "E", number: "51" },
+    { letter: "F", number: "12" },
+    { letter: "G", number: "22" },
+    { letter: "H", number: "32" },
+    { letter: "I/J", number: "42" },
+    { letter: "K", number: "52" },
+    { letter: "L", number: "13" },
+    { letter: "M", number: "23" },
+    { letter: "N", number: "33" },
+    { letter: "O", number: "43" },
+    { letter: "P", number: "53" },
+    { letter: "Q", number: "14" },
+    { letter: "R", number: "24" },
+    { letter: "S", number: "34" },
+    { letter: "T", number: "44" },
+    { letter: "U", number: "54" },
+    { letter: "V", number: "15" },
+    { letter: "W", number: "25" },
+    { letter: "X", number: "35" },
+    { letter: "Y", number: "45" },
+    { letter: "Z", number: "55" },
+  ];
 
   function polybius(input, encode = true) {
-    // your solution code here
+    input = input.toUpperCase();
+    return encode ? _encode(input, square) : _decode(input, square);
   }
 
+  const _encode = (input, squareObjs) => {
+    let result = "";
+    for (let selected of input) {
+      const numObj = squareObjs.find((element) => {
+        return element.letter.includes(selected);
+      });
+      numObj ? (result += numObj.number) : (result += " ");
+    }
+    return result;
+  };
+
+  // Oh hi = 4332 3242
+  const _decode = (input, squareObjs) => {
+    const noSpaces = input.replace(" ", "");
+    if (!(noSpaces.length % 2 === 0)) return false;
+    let result = "";
+    let temp = "";
+    for (let selected of input) {
+      if (selected === " ") result += selected;
+      if (temp.length <= 1 && selected !== " ") temp += selected;
+      if (temp.length === 2) {
+        const letterObj = squareObjs.find((element) => element.number == temp);
+        result += letterObj.letter;
+        temp = "";
+      }
+    }
+    return result.toLowerCase();
+  };
+
+  console.log(_decode("4332 3242", square));
+  // polybius("hello world", true);
   return {
     polybius,
   };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -6,37 +6,45 @@ describe("caesar()", () => {
     const actual = caesar("hello", 0, true);
     expect(actual).to.be.false;
   });
+
   it("should return false if shift is less than -25", () => {
     const actual = caesar("hello", -50, true);
     expect(actual).to.be.false;
   });
+
   it("should return false if shift is greater than 25", () => {
     const actual = caesar("hello", 50, true);
     expect(actual).to.be.false;
   });
+
   it("should return false if shift does not exist", () => {
     const actual = caesar("hello", undefined, true);
     expect(actual).to.be.false;
   });
+
   it("should treat capital letters as lowercase, and return only lowercase", () => {
     const expected = "khoor zruog";
     const actual = caesar("HELLO WORLD", 3, true);
     expect(actual).to.equal(expected);
   });
+
   it("should wrap around to the front or back of the alphabet", () => {
     const expected = "c";
     const actual = caesar("z", 3, true);
     expect(actual).to.equal(expected);
   });
+
   it("should ignore non-alphabetic characters", () => {
     const expected = "!@#     $%^&*()<>  ,.{}[];:'/?|=+-_~";
     const actual = caesar("!@#     $%^&*()<>  ,.{}[];:'/?|=+-_~", 7, true);
     expect(actual).to.equal(expected);
   });
+
   it("should return false if there is no input", () => {
     const actual = caesar(undefined, 5, true);
     expect(actual).to.be.false;
   });
+
   it("should decode input if encode is false", () => {
     const expected = "hello world";
     const actual = caesar("ifmmp xpsme", -25, false);
