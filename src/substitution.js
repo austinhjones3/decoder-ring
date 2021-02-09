67d51a79b2bc9f654f1d07e304415cd10a74cda8
austinhjones3 <austin@austinjones.io>
2021-02-09 16:36:00 -0600

    documented src/substitution

--- src/substitution.js
+++ src/substitution.js
@@ -1,48 +1,78 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-
+  /**
+   * @function substitution()
+   * @param {String} message - to be decoded/encoded
+   * @param {String} alphaInput - new alphabet used to encode or decode
+   * @param {Boolean} encode - tells whether we will encode or decode
+   * @returns {Boolean or String} - either returns false or the
+   * encoded/decoded message
+   */
   function substitution(message, alphaInput, encode = true) {
+    // check arguments for req spec defined false returns
     if (!alphaInput || alphaInput.length !== 26 || !_isUnique(alphaInput)) {
       return false;
     }
+    // move encrypting alpha and message to lowercase so we can encode/decode correctly
     alphaInput = alphaInput.toLowerCase();
     message = message.toLowerCase();
+    // get data to encode/decode with
     const data = _data(alphaInput);
+    // if encode is true, encode, If not, decode
     return encode ? _encode(message, data) : _decode(message, data);
   }
 
+  /**
+   * @function _encode()
+   * @param {String} message - to be encoded
+   * @param {Object[]} data - to encode with
+   * @returns {String} - encoded message
+   */
   const _encode = (message, data) => {
     let output = "";
     for (let selected of message) {
+      // get the obj that matches selected to push encoded part into output
       const newCharObj = data.find((element) => {
         return element.alphaChar.includes(selected);
       });
+      // if it's found, add encoded char to output, else, add selected to output
       newCharObj ? (output += newCharObj.newChar) : (output += selected);
     }
     return output;
   };
 
+  /**
+   * @function _decode()
+   * @param {String} message - to be decoded
+   * @param {Object[]} data - to decode with
+   * @returns {String} - decoded message
+   */
   const _decode = (message, data) => {
     let output = "";
     for (let selected of message) {
+      // get the obj that matches selected to push decoded part into output
       const alphaObj = data.find((element) => {
         return element.newChar.includes(selected);
       });
+      // if it's found, add decoded char to output, else, add selected to output
       alphaObj ? (output += alphaObj.alphaChar) : (output += selected);
     }
     return output;
   };
 
+  /**
+   * @function _isUnique()
+   * @param {String} str - string to check if unique
+   * @returns {Boolean} - true if each character in the string is unique
+   * false if not unique
+   */
   const _isUnique = (str) => {
     const len = str.length;
     for (let i = 0; i < len; i++) {
+      // store current char in temp to compare to next char
       const temp = str[i];
+      // j = the char after charAt(i)
       for (let j = i + 1; j <= len - 1; j++) {
+        // if true, the string is not unique
         if (temp == str[j]) {
           return false;
         }
@@ -51,10 +81,18 @@ const substitutionModule = (function () {
     return true;
   };
 
+  /**
+   * @function _data()
+   * @param {String} alphaInput - the encoded alphabet
+   * @returns {Object[]} - the data needed to encode/decode with. Includes
+   * regular alphabet and encoded alphabet
+   */
   const _data = (alphaInput) => {
     const output = [];
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    // select each character in both alphabets with i
     for (let i = 0; i < alphaInput.length; i++) {
+      // push both chars into the array as an object for accessibility convenience
       output.push({ newChar: alphaInput[i], alphaChar: alphabet.charAt(i) });
     }
     return output;
